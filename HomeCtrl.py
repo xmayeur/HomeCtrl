d1ed81afa27f9731cc22d056801a3cfe3150812b
Xavier Mayeur <xavier@mayeur.be>
2019-03-24 22:28:24 +0100

    initial

--- HomeCtrl.py
+++ HomeCtrl.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-import socket
+# import socket
 import requests
 from time import sleep
 import sys
@@ -7,6 +7,7 @@ import sys
 HOST = '192.168.0.4'
 vault_url = 'http://192.168.0.4:5000/api/ID'
 uid = 'iot'
+hname = 'homectrl'
 
 
 def get_vault(uid):
@@ -25,7 +26,6 @@ def get_vault(uid):
 
 
 def on_message(client, userdata, message):
-    hname = socket.gethostname()
     topic = message.topic
     msg = str(message.payload.decode('utf-8'))
     print('Received message: ' + topic + '/' + msg)
@@ -33,11 +33,16 @@ def on_message(client, userdata, message):
         client.publish(hname + '/status', 'alive', qos=0, retain=False)
 
 
+def on_subscribe(client, userdata, mid, qos):
+    print('Subscribed: ' + str(mid))
+
+
 def on_connect(client, userdata, flags, rc):
     global connect_flag
     if rc == 0:
         print("connected ok")
         connect_flag = True
+        client.subscribe("/SonOff_B4/#")
 
 
 def publish_msg(client, topic, msg):
@@ -63,14 +68,15 @@ def publish_msg(client, topic, msg):
 def main():
     global connect_flag
     uname, pwd = get_vault(uid)
-    hname = socket.gethostname()
     client = mqtt.Client('home_' + hname)
     client.username_pw_set(username=uname, password=pwd)
     
     # client.subscribe(hname + '/getStatus')
-    client.subscribe("/SonOff_B4/relay/state")
+    
     client.on_message = on_message
+    client.on_subscribe = on_subscribe
     client.on_connect = on_connect
+    
     client.loop_start()
     
     connect_flag = False
@@ -86,15 +92,18 @@ def main():
             connect_flag = False
             sys.exit(1)
       
-    # do something
-    topic = '/SonOff_B4/gpio/12'
-    msg = 1
-    print(1)
-    publish_msg(client, topic, msg)
-    sleep(5)
-    msg = 0
-    print(0)
-    publish_msg(client, topic, msg)
+    while True:
+        # do something
+        topic = '/SonOff_B4/gpio/12'
+        msg = '1'
+        print(1)
+        publish_msg(client, topic, msg)
+        sleep(5)
+        topic = '/SonOff_B4/gpio/12'
+        msg = '0'
+        publish_msg(client, topic, msg)
+        print(0)
+        sleep(5)
 
 
 if __name__ == '__main__':
